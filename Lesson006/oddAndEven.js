7b73ccc02236c1c4c6a467ddc7a051a5f9d19a5f
KarlNersisyan <karlennersisyan814@gmail.com>
2021-05-04 14:03:53 +0400

    feat(Lesson006):superate odd & even arrays

--- Lesson006/oddAndEven.js
+++ Lesson006/oddAndEven.js
@@ -4,13 +4,13 @@ function separateNumbers(array) {
 
   for (let i = 0; i < array.length; i++) {
     if (array[i] % 2 === 0) {
-      odd.push(array[i]);
-    } else {
       even.push(array[i]);
+    } else {
+      odd.push(array[i]);
     }
   }
 
-  return [odd, even];
+  return [even, odd];
 }
 
 console.log(separateNumbers([45, 630, 1, 4]));
