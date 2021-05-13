b5f1abfad8a98cc51a23b2eb3b3a4ef4c2d686bd
KarlNersisyan <karlennersisyan814@gmail.com>
2021-05-13 10:09:55 +0400

    feat(Lesson0010):to Sort

---
+++ Lesson0010/sortMethod5.js
@@ -0,0 +1,22 @@
+/*- Create a function that takes an array of numbers arr, a string str
+and return an array of numbers as per the following rules:
+- "Asc" returns a sorted array in ascending order.
+- "Desc" returns a sorted array in descending order.
+``` javascript
+sortBy([4, 3, 2, 1], "Asc" ) // [1, 2, 3, 4]
+sortBy([7, 8, 11, 66]) // [7, 8, 11, 66]
+sortBy([7, 8, 11, 66], "Desc") // [66, 11, 8, 7]
+```*/
+function sortBy(arr, str) {
+  if (str === "Desc") {
+    console.log(arr.sort((a, b) => b - a));
+  } else if (str === "Asc") {
+    console.log(arr.sort((a, b) => a - b));
+  } else {
+    console.log(arr.sort((a, b) => a - b));
+  }
+}
+
+sortBy([4, 3, 2, 1], "Asc"); // [1, 2, 3, 4]
+sortBy([7, 8, 11, 66]); // [7, 8, 11, 66]
+sortBy([7, 8, 11, 66], "Desc"); // [66, 11, 8, 7]
