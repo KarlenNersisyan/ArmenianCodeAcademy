3ce908e11b5c3566de8289aa4635432a7b242b7d
KarlNersisyan <karlennersisyan814@gmail.com>
2021-05-17 23:55:41 +0400

    feat(Lesson0013):Implement following function

---
+++ Lesson0013/implement6.js
@@ -0,0 +1,16 @@
+/*
+    Implement following function
+
+const getWeekOfMonth = () => {};
+const result = getWeekOfMonth(new Date(2017, 10, 9));
+// => 2
+*/
+
+const getWeekOfMonth = (date) => {
+  let prefixes = [1, 2, 3, 4, 5];
+
+  return prefixes[Math.floor(date.getDate() / 7)];
+};
+
+const result = getWeekOfMonth(new Date(2017, 10, 9));
+console.log(result);
