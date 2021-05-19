baa2adfed15e21e33da4a27aa1d78ab0e483c8e8
KarlNersisyan <karlennersisyan814@gmail.com>
2021-05-19 18:15:56 +0400

    feat(Lesson0015): Aircraft factory

---
+++ Lesson0015/airplane2.js
@@ -0,0 +1,26 @@
+/*
+Airplane TASK:
+Write an Airplane object that initializes name.
+Give airplanes the ability to .takeOff() and .land():
+If a plane takes off, its isFlying property is set to true.
+If a plane lands, its isFlying property is set to false.
+*/
+
+let Airplane = {
+  isFlying: false,
+
+  takeOff: (obj) => {
+    obj.isFlying = true;
+  },
+  land: (obj) => {
+    obj.isFlying = false;
+  },
+};
+const Singapore = Object.create(Airplane);
+const Armenia = Object.create(Airplane);
+
+Singapore.takeOff(Singapore);
+console.log("Singapore:", Singapore.isFlying);
+Armenia.takeOff(Armenia);
+Armenia.land(Armenia);
+console.log("Armenia:", Armenia.isFlying);
