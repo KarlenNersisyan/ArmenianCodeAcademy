5eb5714c9bd9d47634b00a7e57ec287e144d284a
KarlNersisyan <karlennersisyan814@gmail.com>
2021-05-24 15:36:55 +0400

    feat(Lesson0016): Baby_Constructor

--- Lesson0016/babyConstructor.js
+++ Lesson0016/babyConstructor.js
@@ -1,52 +1,20 @@
-function Person({ name, age, stomach = [] } = {}) {
+function Person({ name, age }) {
   this.name = name;
   this.age = age;
-  this.stomach = stomach;
-
-  //   this.eat = function (...food) {
-  //     console.log(food);
-  //     this.stomach = [...this.stomach, ...food];
-  //   };
 }
 
-Person.prototype.eat = function (...food) {
-  if (this.stomach.length < 10) {
-    this.stomach = [...this.stomach, ...food];
-  } else {
-    throw new Error("Too Much Food!");
-  }
-};
-
-Person.prototype.poop = function () {
-  this.stomach = [];
-};
-
-Person.prototype.toString = function () {
-  return `Person : ${this.name} - ${this.age}`;
-};
-
-const rawUser = { name: "John", age: "44" };
-
-const u1 = new Person(rawUser);
-const u2 = new Person();
-
-u1.eat("pizza", "fries");
-u1.eat("pasta");
-
-console.log(u1);
-console.log(u2);
-
 function Baby(name, age, favoriteToys = []) {
-  //   console.log("PRE::", this);
   Person.call(this, { name, age });
-  //   console.log("POST::", this);
   this.favoriteToys = favoriteToys;
 }
 
 Baby.prototype = Object.create(Person.prototype);
 
-const b1 = new Baby("Tom", 1);
-console.log(b1);
+Baby.prototype.play = function () {
+  return `Playing with ${this.favoriteToys}`;
+};
 
-b1.eat("milk");
-console.log(b1.toString());
+const b1 = new Baby("Tom", 1, "car");
+
+console.log(b1);
+console.log(b1.play());
