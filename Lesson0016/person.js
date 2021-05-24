13b744520bc90f0982565544a818360a30cedd9a
KarlNersisyan <karlennersisyan814@gmail.com>
2021-05-24 15:50:25 +0400

    feat(Lesson0016): Person

--- Lesson0016/person.js
+++ Lesson0016/person.js
@@ -5,18 +5,18 @@ function Person(name, age) {
 
 Person.prototype.compareAge = function (anPerson) {
   if (this.age < anPerson.age) {
-    return "Joel is older than me.";
+    return `${p2.name} is older than me.`;
   } else if (this.age > anPerson.age) {
-    return "Samuel is younger than me.";
+    return `${p1.name} is younger than me.`;
   } else {
-    return "Lily is the same age as me.";
+    return `${p3.name} is the same age as me.`;
   }
 };
 
-const p1 = new Person("Samuel", 24);
-const p2 = new Person("Joel", 36);
+const p1 = new Person("Karl", 24);
+const p2 = new Person("John", 36);
 const p3 = new Person("Lily", 24);
 
 console.log(p1.compareAge(p2));
+console.log(p2.compareAge(p1));
 console.log(p1.compareAge(p3));
-console.log(p2.compareAge(p3));
