13a5459be8c6991776345fb390947d4b2a36c6fb
KarlNersisyan <karlennersisyan814@gmail.com>
2021-05-30 18:45:09 +0400

    feat(Lesson0018): Fibonacci Numbers recursion

--- Lesson0018/fibonacciNumbers3.js
+++ Lesson0018/fibonacciNumbers3.js
@@ -1,6 +1,9 @@
 /* Write a fib(int) function which returns n-th fibonacci number. */
 
 function fibonacci(n) {
-  return n <= 1 ? n : fibonacci(n - 1) + fibonacci(n - 2);
+  if (n < 0) {
+    return "Enter '0' or a positive integer";
+  }
+  return n === 0 || n === 1 ? n : fibonacci(n - 1) + fibonacci(n - 2);
 }
 console.log(fibonacci(6)); // 1,1,2,3,5,8,13,21,34 ... -> 8
