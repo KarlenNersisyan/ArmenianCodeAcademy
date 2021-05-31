8ba50011e8e823a6a5469c29c80be1be6434330c
KarlNersisyan <karlennersisyan814@gmail.com>
2021-05-31 16:28:28 +0400

    feat(Lesson0018):Separation Of Brackets

---
+++ Lesson0018/separationOfBrackets4.js
@@ -0,0 +1,32 @@
+/*
+
+Add brackets between letters.
+Given a string containing only English letters (uppercase and lowercase). Add opening and closing parentheses
+according to the following pattern: "example" -> "(e (x (a (m) p) l) e)" (Added opening parentheses to the
+middle, closing parentheses after the middle. In case the string length is even there must be 2 characters in the
+brackets in the middle. ("card -> (c (ar) d" but not "(c (a () r) d)"). Example`
+
+*/
+
+let word = "Karl";
+const addBrackets = (word, result = "", i = 0) => {
+  if (word[i] === undefined) {
+    return result;
+  }
+  let middlePart = Math.floor(word.length / 2);
+  if (i < middlePart) {
+    result += "(" + word[i];
+  }
+  if (i === middlePart) {
+    result += word.length % 2 === 0 ? word[i] + ")" : "(" + word[i] + ")";
+  }
+  if (i > middlePart) {
+    result += word[i] + ")";
+  }
+
+  i++;
+
+  return addBrackets(word, result, i);
+};
+
+console.log(addBrackets(word)); // (K(ar)l)
