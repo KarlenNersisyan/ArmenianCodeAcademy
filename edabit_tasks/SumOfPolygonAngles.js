a19305dd666feea0d2beec0a2811456bd128fd13
KarlNersisyan <karlennersisyan814@gmail.com>
2021-06-01 13:33:45 +0400

    feat(edabit_tasks): Sum Of Polygon Angles

---
+++ edabit_tasks/SumOfPolygonAngles.js
@@ -0,0 +1,7 @@
+/* Given an n-sided regular polygon n, return the total sum of internal angles (in degrees). */
+
+function sumPolygon(n) {
+  return (n - 2) * 180;
+}
+
+console.log(sumPolygon(6)); // 720
